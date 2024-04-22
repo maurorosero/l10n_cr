caf1c08cf5d0cda9509b74079809ab6b925a243c
Mauro Rosero P <mauro@rosero.one>
2024-04-21 20:30:06 -0500

    [FIXED] Se mejoró la funcionalidad de visualizar el log generado y se renombró la función.

--- logs.sh
+++ logs.sh
@@ -1,24 +1,30 @@
-#  logview.sh
-#  
+##  logs.sh
+#
 #  Mauro Rosero P. <mrosero@rosero.one>
-#  
+#
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 2 of the License, or
 #  (at your option) any later version.
-#  
+#
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#  
+#
 #  You should have received a copy of the GNU General Public License
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
-#  
-#  
-clear
-echo "Odoo Developers Instance (View running log)"
-echo "==============================================" 
-tail -f etc/odoo.log
+#
+#
+
+display_header() {
+  clear
+  echo "$(cat < ./config/project.head) Logger"
+  echo "=========================================="
+}
+
+# MAIN PROGRAM
+display_header
+tail -f etc/odoo.log | less
